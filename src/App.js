dcb5dd87ce2e1ede68b3835faaeb40713c0f2ed4
shweta1112 <shweta.cs0927@gmail.com>
2019-10-01 16:15:51 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function asyncHOC(CustomComponent, url) {
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        componentData: []
+      };
+    }
+    componentDidMount() {
+      fetch(url)
+        .then(resp => resp.json())
+        .then(response => {
+          if (response.length) {
+            this.setState({
+              componentData: response
+            });
+          }
+        });
+    }
+    render() {
+      const getAPIDate = () => {
+        return <CustomComponent componentData={this.state.componentData} />;
+      };
+      return (
+        <div>
+          <h1>API Data:</h1>
+          {this.state.componentData.length ? (
+            getAPIDate()
+          ) : (
+            <div>Loading.....</div>
+          )}
+        </div>
+      );
+    }
+  };
 }
-
+const RenderComponent = props => {
+  return props.componentData.map(e => {
+    return (
+      <div>
+        <div>EMPLOYEE_ID: {e.id}</div>
+        <div>EMPLOYEE_NAME: {e.employee_name}</div>
+        <div>EMPLOYEE_SALARY: {e.employee_salary}</div>
+        <div>EMPLOYEE_AGE: {e.employee_age}</div>
+      </div>
+    );
+  });
+};
+const App = asyncHOC(
+  RenderComponent,
+  "http://dummy.restapiexample.com/api/v1/employees"
+);
 export default App;
